cf23d5021dc58cb43305218fe7009c6ec8e48ca2
Edima <edimaudo@gmail.com>
2025-05-22 16:45:12 +0000

    Updated About

--- About.py
+++ About.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="About",
+    page_icon="👋",
+)
+
+st.title("Dr Know")
+st.header("About")
+
+st.markdown(
+    """
+    Human beings are wonderful but we can get run down by aches, pains and sickness.    
+    Dr Know provides a high level diagnostic tool that people can use in the convience of their homes to understand issues they may have.
+    It is not a replacement for a doctor but a conversation starter.  
+    It is built using [streamlit](https://streamlit.io/cloud) and [Gemini](https://aistudio.google.com/).
+    """
 )
