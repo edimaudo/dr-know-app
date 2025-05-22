5bb858829020843c72736e0827b2f9d17a07bdca
Edima <edimaudo@gmail.com>
2025-05-22 17:00:35 +0000

    updated about file

--- About.py
+++ About.py
@@ -11,7 +11,7 @@ st.header("About")
 st.markdown(
     """
     Human beings are wonderful but we can get run down by aches, pains and sickness.    
-    Dr Know provides a high level diagnostic tool that people can use in the convience of their homes to understand issues they may have.
+    Dr Know provides a high level health diagnostic tool that people can use in the convenience of their homes to understand health issues they may have.
     It is not a replacement for a doctor but a conversation starter.  
     It is built using [streamlit](https://streamlit.io/cloud) and [Gemini](https://aistudio.google.com/).
     """
